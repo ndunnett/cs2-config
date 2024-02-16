b93a168ffc0edc6bd596a77a6a0001dd5a5f9572
ndunnett <46371520+ndunnett@users.noreply.github.com>
2024-02-17 04:11:21 +1000

    Add update_cs2_video.ps1

---
+++ update_cs2_video.ps1
@@ -0,0 +1,43 @@
+# script to programmatically apply optimal shadow parameters to cs2_video.txt
+# written by ndunnett - https://github.com/ndunnett/cs2-config
+
+# location of your Steam userdata directory
+$userdata = "C:\Program Files (x86)\Steam\userdata"
+
+# parameters to apply
+$parameters = @{
+    "setting.csm_viewmodel_shadows" = 1;
+    "setting.csm_max_shadow_dist_override" = 720;
+    "setting.lb_barnlight_shadowmap_scale" = 0.4;
+    "setting.lb_enable_shadow_casting" = 1;
+}
+
+if (-not (Test-Path -Path $userdata -PathType Container)) {
+    Write-Host "Directory doesn't exist: $userdata"
+    Write-Host "Ensure `$userdata is correctly set to your Steam userdata directory."
+} else {
+    foreach ($file in Get-ChildItem "$userdata\*\730\local\cfg\cs2_video.txt") {
+        Write-Host "Processing $file..."
+        $content = Get-Content -Path $file.FullName
+
+        $parameters.GetEnumerator().foreach({
+            foreach ($line in $content) {
+                if ($line -match "$($_.Key)") {
+                    if ($line -match "`"$($_.Key)`"\s+`"$($_.Value)`"") {
+                        Write-Host " - Already set: $($_.Key) = $($_.Value)"
+                    } else {
+                        Write-Host " - Changed: $($_.Key) = $($_.Value)"
+                        $line = $line -replace "`"(.*)`"(\s+)`"(.*)`"", "`"`$1`"`$2`"$($_.Value)`""
+                    }
+                }
+            }
+        })
+
+        $content | Set-Content -Path $file.FullName
+        Write-Host ""
+    }
+
+    Write-Host "Done!"
+}
+
+PAUSE
